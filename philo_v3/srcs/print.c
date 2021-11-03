80fb7691807e2f2dc7eac1f3e5545d0ddf4bee2e
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-11-03 20:18:23 +0100

    Reimplemented my print.c code, but made the wrapper work properly, i.e. it now checks whether it should continue with printing before printing, btu does this in two different ways depending on whether the function that called it already holds the mutex it requires or not (this is specified by the parameter 'mode').

--- philo_v3/srcs/print.c
+++ philo_v3/srcs/print.c
@@ -6,52 +6,45 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/21 17:42:59 by limartin      #+#    #+#                 */
-/*   Updated: 2021/11/02 17:15:29 by limartin      ########   odam.nl         */
+/*   Updated: 2021/11/03 19:28:17 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-// int	ft_print_status(t_data *d, t_state reason, int philo_id, int unused)
-// {
-// 	int	ms;
+int	cp_print_status(t_data *d, t_state reason, int philo_id, char mode)
+{
+	int	ms;
 
-// 	(void)unused;
-// 	ms = ft_get_ms(d);
-// 	if (reason == _ded)
-// 	{
-// 		printf(RED "%d Philo %d died\n" DEF, ms, philo_id);
-// 		ft_game_over(d, philo_id);
-// 	}
-// 	else if (reason == _fork)
-// 		printf(YLW "%d Philo %d has taken a fork\n" DEF, ms, philo_id);
-// 	else if (reason == _eat)
-// 	{
-// 		printf(YLW "%d Philo %d has taken a fork\n" DEF, ms, philo_id);
-// 		printf(GREEN "%d Philo %d is eating\n" DEF, ms, philo_id);
-// 	}
-// 	else if (reason == _sleep)
-// 		printf(BLUE "%d Philo %d is sleeping\n" DEF, ms, philo_id);
-// 	else if (reason == _think)
-// 		printf(CYAN "%d Philo %d is thinking\n" DEF, ms, philo_id);
-// 	return (0);
-// }
+	ms = ft_get_ms(d);
+	if (reason == _ded)
+	{
+		printf(RED "%d Philo %d died\n" DEF, ms, philo_id);
+		cp_game_over(d, mode);
+	}
+	else if (reason == _fork)
+		printf(YLW "%d Philo %d has taken a fork\n" DEF, ms, philo_id);
+	else if (reason == _eat)
+	{
+		printf(YLW "%d Philo %d has taken a fork\n" DEF, ms, philo_id);
+		printf(GREEN "%d Philo %d is eating\n" DEF, ms, philo_id);
+	}
+	else if (reason == _sleep)
+		printf(BLUE "%d Philo %d is sleeping\n" DEF, ms, philo_id);
+	else if (reason == _think)
+		printf(CYAN "%d Philo %d is thinking\n" DEF, ms, philo_id);
+	return (0);
+}
 
-// int ft_request_print(t_data *d, t_state reason, int philo_id, int authority)
-// {
-// 	if (authority == 0)
-// 	{
-// 		pthread_mutex_lock(&(d->print_status));
-// 		if (ft_continue(d, -1))
-// 			ft_print_status(d, reason, philo_id, authority);
-// 		pthread_mutex_unlock(&(d->print_status));
-// 	}
-// 	else if (authority == 1)
-// 	{
-// 		pthread_mutex_lock(&(d->print_status));
-// 		if (!(d->terminate))
-// 			ft_print_status(d, reason, philo_id, authority);
-// 		pthread_mutex_unlock(&(d->print_status));
-// 	}
-// 	return (0);
-// }
+int cp_request_print(t_data *d, t_state reason, int philo_id, char mode)
+{
+	int go_ret;
+
+	go_ret = 0;
+	pthread_mutex_lock(&(d->print_status));
+	go_ret = cp_continue(d, mode);
+	if (go_ret)
+		cp_print_status(d, reason, philo_id, mode);
+	pthread_mutex_unlock(&(d->print_status));
+	return (go_ret);
+}
