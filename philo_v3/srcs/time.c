c1be030d0828556087a90a58a8dd1519eed7f8f8
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-11-03 20:14:20 +0100

    Readded the code in time.c without changes, though may make a more accurate usleep for when it actually matters.

--- philo_v3/srcs/time.c
+++ philo_v3/srcs/time.c
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/29 15:08:19 by limartin      #+#    #+#                 */
-/*   Updated: 2021/11/03 15:44:49 by limartin      ########   odam.nl         */
+/*   Updated: 2021/11/03 19:03:24 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@ int	ft_start_clock(t_data *d)
 	return (0);
 }
 
-// int	ft_get_ms(t_data *d)
-// {
-// 	int				s;
-// 	int				microS;
-// 	int				miniS;
-// 	struct timeval	now;
+int	ft_get_ms(t_data *d)
+{
+	int				s;
+	int				microS;
+	int				miniS;
+	struct timeval	now;
 
-// 	gettimeofday(&now, NULL);
-// 	s = (now.tv_sec - d->start_time.tv_sec);
-// 	microS = (now.tv_usec - d->start_time.tv_usec);
-// 	miniS = s * 1000 + microS / 1000;
-// 	return ((int)miniS);
-// }
+	gettimeofday(&now, NULL);
+	s = (now.tv_sec - d->start_time.tv_sec);
+	microS = (now.tv_usec - d->start_time.tv_usec);
+	miniS = s * 1000 + microS / 1000;
+	return ((int)miniS);
+}
