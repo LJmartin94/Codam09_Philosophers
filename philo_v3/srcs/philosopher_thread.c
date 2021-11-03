c88f9cee2f77c84a1001a95c3d449b6eb959fb72
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-11-03 21:34:07 +0100

    Added a death checker to the behavioural loop, as it was easy enough to do and doesnt increase the mutex load.

--- philo_v3/srcs/philosopher_thread.c
+++ philo_v3/srcs/philosopher_thread.c
@@ -6,11 +6,18 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/12 23:10:22 by limartin      #+#    #+#                 */
-/*   Updated: 2021/11/03 20:46:02 by limartin      ########   odam.nl         */
+/*   Updated: 2021/11/03 21:30:01 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+int	c_dead(t_philo_thread_args *pta)
+{
+	pta->state = _ded;
+	cp_request_print(pta->d, pta->state, pta->philo, 'c');
+	return (0);
+}
 
 int	c_sleep(t_philo_thread_args *pta)
 {
@@ -36,6 +43,8 @@ void	c_behavioural_loop(t_philo_thread_args *pta, int *last_ate, int *go)
 	int	now;
 
 	now = ft_get_ms(pta->d); //could still check for death in this loop
+	if (now >= (*last_ate + pta->d->time_to_die))
+		*go = c_dead(pta);
 	if (now >= (*last_ate + pta->d->time_to_eat) && pta->state == _eat)
 		*go = c_sleep(pta);
 	else if (now >= (*last_ate + pta->d->time_to_eat + pta->d->time_to_sleep) \
@@ -55,11 +64,12 @@ void	*ft_philosophise(void *args)
 	pta->philo = pta->thread_id + 1;
 	pta->state = _think;
 	local_last_ate = 0;
-	go = 1;
+	go = cp_continue(pta->d, 'c');
 	while (go) //replaced 'cp_continue(pta->d, 'c')' with 'go'
 	{
 		c_behavioural_loop(pta, &local_last_ate, &go);
-// 		usleep(100 + (pta->d->number_of_philosophers * 3 / 4));
+		// usleep(100 + (pta->d->number_of_philosophers * 3 / 4));
+		usleep(250);
 		if(pta->d->notepme > 0 && pta->times_ate >= pta->d->notepme)
 			go = cp_continue(pta->d, 'c');
 	}
